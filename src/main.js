ca11870f843af834e0d31b21a868f4f79bde8c06
KentChen <bboykunz@gmail.com>
2023-05-15 12:33:07 +0800

    Build basic environment

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import obj from './utils.js';
+
+let input = 30;
+console.log(`輸入數字：${input}`);
+console.log(`經過utils.js得到的結果：${obj.cal(input)}`);
+console.log(`obj名稱：${obj.name}`);
---
+++ src/utils.js
@@ -0,0 +1,8 @@
+function calculate(n) {
+    return (n * 3 + 15) / 10 - 3;
+}
+
+export default {
+    cal: calculate,
+    name: 'hello'
+}
